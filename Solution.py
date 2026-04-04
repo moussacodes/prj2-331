2be7d75f8e426b58992025392d6a161fd3ce550b
PapaDoDaCode <mrpapadiene@gmail.com>
2026-04-03 22:26:55 -0400

    dijk's algo for output_paths, passes test case with long runtime

--- Solution.py
+++ Solution.py
@@ -4,8 +4,9 @@ from collections import deque
 from Simulator import Simulator
 import sys
 
-class Solution:
+import heapq
 
+class Solution:
     def __init__(self, problem, isp, graph, info):
         self.problem = problem
         self.isp = isp
@@ -13,11 +14,61 @@ class Solution:
         self.info = info
 
     def output_paths(self):
-        """
-        This method must be filled in by you. You may add other methods and subclasses as you see fit,
-        but they must remain within the Solution class.
-        """
         paths, bandwidths, priorities = {}, {}, {}
-        # Note: You do not need to modify all of the above. For Problem 1, only the paths variable needs to be modified. If you do modify a variable you are not supposed to, you might notice different revenues outputted by the Driver locally since the autograder will ignore the variables not relevant for the problem.
-        # WARNING: DO NOT MODIFY THE LINE BELOW, OR BAD THINGS WILL HAPPEN
+
+        list_clients = self.info["list_clients"]
+        payments = self.info["payments"]
+        bws = self.info["bandwidths"]
+
+        def get_payment_amount(client):
+            return payments.get(client, 0)
+        
+        paying_clients = [c for c in list_clients if get_payment_amount(c) > 0]
+        sorted_clients = sorted(paying_clients, key=get_payment_amount, reverse=True)
+
+        node_usage = {node: 0 for node in self.graph}
+
+        penalty_mult = {}
+        for v, b in bws.items():
+            if b > 0 and b != float('inf'):
+                penalty_mult[v] = 2.5 / b
+
+        for client in sorted_clients:
+            pq = [(0, self.isp)]
+            dist = {self.isp: 0}
+            parent = {self.isp: None}
+
+            while pq:
+                d, u = heapq.heappop(pq)
+
+                if d > dist.get(u, float('inf')):
+                    continue
+
+                if u == client:
+                    break
+
+                for v in self.graph[u]:
+                    cost_v = 1
+                    
+                    if v in penalty_mult:
+                        cost_v += node_usage[v] * penalty_mult[v]
+
+                    new_dist = d + cost_v
+                    if new_dist < dist.get(v, float('inf')):
+                        dist[v] = new_dist
+                        parent[v] = u
+                        heapq.heappush(pq, (new_dist, v))
+
+            path = []
+            curr = client
+            while curr is not None:
+                path.append(curr)
+                curr = parent.get(curr)
+            
+            path.reverse()
+            paths[client] = path
+
+            for node in path:
+                node_usage[node] += 1
+
         return (paths, bandwidths, priorities)
